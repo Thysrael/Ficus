0bd3dce90a90ff5cc483c7beb8ba9942296acc58
Blore <1137480882@qq.com>
2023-04-10 23:57:24 +0800

    feat(ir): 完成部分md转IR

--- src/IR/block/base/treeNode.ts
+++ src/IR/block/base/treeNode.ts
@@ -3,8 +3,22 @@ import LinkedNode from './linkedList/linkedNode'
 
 class TreeNode extends LinkedNode {
   private nodeType: BaseNodeType
-  constructor (nodeType) {
+  private classList: string[]
+  private attributes: {}
+  private datasets: {}
+
+  constructor (nodeType: BaseNodeType) {
     super()
     this.nodeType = nodeType
+    // DOM相关
+    this.classList = []
+    this.attributes = {}
+    this.datasets = {}
+  }
+
+  public isLeafNode(): boolean {
+    return this.nodeType.isLeaf()
   }
 }
+
+export default TreeNode
