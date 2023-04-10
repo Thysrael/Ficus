e6f400e4dc9e7cc204b565de0d1afcad4e2367e1
GwokHiujin <guozi1625113688@163.com>
2023-04-10 18:37:26 +0800

    feat(*.config.js): 添加了Tailwind CSS相关全局配置

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,6 @@
 // 定义共用常量
 const CONSTANTS = Object.freeze({
-    DEFAULT: 'to be deleted'
+  DEFAULT: 'to be deleted'
 })
 
 export default CONSTANTS
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './renderer/App.vue'
+import './renderer/assets/index.css'
 
 createApp(App).mount('#app')
