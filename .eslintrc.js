26df4d6b3d4d61553059c1fd37aaeded9cc1eacb
DESKTOP-415OV3U\dofingert <dofingert@gmail.com>
2023-04-11 13:55:37 +0800

    fix(.eslintrc): 暂时不对 ts 进行语法检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,21 +7,15 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'standard',
-    // 启用eslint推荐的规则
-    'eslint:recommended',
-    // typescript-eslint插件的检验规则
-    'plugin:@typescript-eslint/recommended'
+    'standard'
   ],
-  parser: '@typescript-eslint/parser',
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   plugins: [
-    'vue',
-    '@typescript-eslint'
+    'vue'
   ],
   rules: {
   }
