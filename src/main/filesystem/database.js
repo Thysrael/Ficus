a621610a58a3585e4d389c592c530b6e6bd12576
Bruce <brucejrue25@gmail.com>
2023-04-13 20:49:59 +0800

    feat(backend): 添加与前端交互的端口

--- src/main/filesystem/database.js
+++ src/main/filesystem/database.js
@@ -1,9 +1,11 @@
 const { app, dialog } = require('electron')
 const fs = require('fs-extra')
 const path = require('path')
+const { getTree } = require('./getFileTree')
+
 // 新建项目的数据库初始化：
 exports.initFromEmptyFolder = async (projName) => {
-  dialog.showOpenDialog({
+  return await dialog.showOpenDialog({
     buttonLabel: '选择',
     defaultPath: app.getPath('desktop'),
     properties: ['createDirectory', 'openDirectory'],
@@ -12,36 +14,41 @@ exports.initFromEmptyFolder = async (projName) => {
       { name: 'Markdown Files', extensions: ['md', 'markdown'] },
       { name: 'images', extensions: ['jpg', 'png'] }
     ]
-  }).then((result) => {
-    console.log(result)
-    return result
-    // const basePath = path.join(result.filePaths[0], '.ficus')
+  }).then(async (result) => {
+    const relation = {
+      version: 1,
+      root: {
+        name: projName,
+        path: result.filePaths[0],
+        tree: {},
+        tags: [],
+        links: []
+      }
+    }
+    const pathSplit = result.filePaths[0].split('\\')
+    const folderName = pathSplit[pathSplit.length - 1]
+    // console.log(result.filePaths[0])
+    const tree = await getTree(result.filePaths[0], folderName)
+    // console.log(tree)
+    relation.root.tree = tree.children.filter(item => item.name !== '.ficus')
+    // console.log(result)
+    const basePath = path.join(result.filePaths[0], '.ficus')
     // console.log(basePath)
-    // fs.mkdir(basePath, { recursive: true }, err => {
-    //   if (err) console.log(`mkdir path: ${basePath} err`)
-    // })
-    // const relationJSONFilePath = path.join(basePath, 'relation.json')
-    // const relation = {
-    //   version: 1,
-    //   root: {
-    //     name: projName,
-    //     path: result.filePaths[0],
-    //     children: [],
-    //     tags: [],
-    //     links: []
-    //   }
-    // }
-    // getTree()
-    // fs.writeFile(relationJSONFilePath, JSON.stringify(relation), (error) => {
-    //   // 创建失败
-    //   if (error) {
-    //     console.log(`创建失败：${error}`)
-    //   }
-    //   // 创建成功
-    //   console.log('创建成功！')
-    // })
-    // const str = JSON.stringify(relation)
-    // return str
+    fs.mkdir(basePath, { recursive: true }, err => {
+      if (err) console.log(`mkdir path: ${basePath} err`)
+    })
+    const relationJSONFilePath = path.join(basePath, 'relation.json')
+
+    fs.writeFile(relationJSONFilePath, JSON.stringify(relation), (error) => {
+      // 创建失败
+      if (error) {
+        console.log(`Fail: ${error}`)
+      }
+      // 创建成功
+      console.log('Success!')
+    })
+    JSON.stringify(relation)
+    return relation
   })
 }
 
