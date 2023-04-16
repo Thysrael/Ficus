3e482410abf0e35e456f87c3a0fdf266cb55fe00
Blore <1137480882@qq.com>
2023-04-16 19:06:26 +0800

    feat(ir): 增加md解析并增加test以及导出功能

---
+++ src/IR/manager/index.js
@@ -0,0 +1,56 @@
+const { markdownToTree } = require('../block/factory/markdownToTree')
+
+class DataManager {
+  constructor () {
+    this.tree = undefined
+    this.forest = undefined
+    this.graph = undefined
+  }
+
+  /**
+     * 从markdown生成FicusTree
+     * @param markdown md文本
+     * @param replaced 是否替换当前tree
+     * @returns
+     */
+  buildTreeFromMarkdown (markdown, replaced = false) {
+    const newTree = markdownToTree(markdown)
+    if (replaced) {
+      this.tree = newTree
+    }
+    return newTree
+  }
+
+  /**
+     * 由当前FicusTree生成md
+     * @returns markdown字符串
+     */
+  getTreeMarkdown () {
+    if (this.tree === undefined) {
+      return ''
+    }
+    return this.tree.toMarkdown()
+  }
+
+  /**
+   *
+   * @returns 大纲json
+   */
+  getTreeOutline () {
+    if (this.tree === undefined) {
+      return {}
+    }
+    return this.tree.toOutlineJson()
+  }
+
+  // /**
+  //    *
+  //    * @param srcNode 原NODE
+  //    * @param dstNode 插入到目标NODE前
+  //    */
+  // copy (srcNode, dstNode) {
+
+  // }
+}
+
+export default DataManager
--- test/IR/markdownToTree.test.js
+++ test/IR/markdownToTree.test.js
@@ -2,8 +2,37 @@ const { markdownToTree } = require('../../src/IR/block/factory/markdownToTree.js
 const { expect } = require('chai')
 
 describe('markdown生成树测试', function () {
-  let root = markdownToTree('aa')
+  let root = markdownToTree('# aa\naa')
   it('简单单行测试', function () {
-    expect(root.toMarkdown()).to.be.equal('aa')
+    expect(root.toMarkdown()).to.be.equal('# aa\n\naa\n\n')
   })
 })
+
+describe('markdown生成树测试', function () {
+  let root = markdownToTree('# a\n## aa\naaa## aa\n### aaa\naaa')
+  it('复合标题段落测试', function () {
+    expect(root.toMarkdown()).to.be.equal('# a\n\n## aa\n\naaa## aa\n\n### aaa\n\naaa\n\n')
+  })
+})
+
+describe('markdown生成树测试', function () {
+  let root = markdownToTree('- 1\n- # 2\n- ## 3')
+  it('无序列表测试', function () {
+    expect(root.toMarkdown()).to.be.equal('- 1\n\n- # 2\n\n- ## 3\n\n')
+  })
+})
+
+describe('markdown生成树测试', function () {
+  let root = markdownToTree('1. 11\n2. 22\n3. 33')
+  it('有序列表测试', function () {
+    expect(root.toMarkdown()).to.be.equal('1. 11\n\n2. 22\n\n3. 33\n\n')
+  })
+})
+
+describe('markdown生成树测试', function () {
+  let root = markdownToTree('- [x] 3\n- [ ] 2')
+  it('任务列表测试', function () {
+    expect(root.toMarkdown()).to.be.equal('- [x] 3\n\n- [ ] 2\n\n')
+  })
+})
+
