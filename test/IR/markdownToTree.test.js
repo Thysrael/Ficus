bf1037e82e16b86da3ec42754add4e9f8a340e7d
Blore <1137480882@qq.com>
2023-04-16 19:25:01 +0800

    style(testir): 修改test格式

--- test/IR/markdownToTree.test.js
+++ test/IR/markdownToTree.test.js
@@ -1,38 +1,25 @@
 const { markdownToTree } = require('../../src/IR/block/factory/markdownToTree.js')
 const { expect } = require('chai')
 
-describe('markdown生成树测试', function () {
-  let root = markdownToTree('# aa\naa')
+describe('markdown生成树简单测试', function () {
+  let root1 = markdownToTree('# aa\naa')
   it('简单单行测试', function () {
-    expect(root.toMarkdown()).to.be.equal('# aa\n\naa\n\n')
+    expect(root1.toMarkdown()).to.be.equal('# aa\n\naa\n\n')
   })
-})
-
-describe('markdown生成树测试', function () {
-  let root = markdownToTree('# a\n## aa\naaa## aa\n### aaa\naaa')
+  let root2 = markdownToTree('# a\n## aa\naaa## aa\n### aaa\naaa')
   it('复合标题段落测试', function () {
-    expect(root.toMarkdown()).to.be.equal('# a\n\n## aa\n\naaa## aa\n\n### aaa\n\naaa\n\n')
+    expect(root2.toMarkdown()).to.be.equal('# a\n\n## aa\n\naaa## aa\n\n### aaa\n\naaa\n\n')
   })
-})
-
-describe('markdown生成树测试', function () {
-  let root = markdownToTree('- 1\n- # 2\n- ## 3')
+  let root3 = markdownToTree('- 1\n- # 2\n- ## 3')
   it('无序列表测试', function () {
-    expect(root.toMarkdown()).to.be.equal('- 1\n\n- # 2\n\n- ## 3\n\n')
+    expect(root3.toMarkdown()).to.be.equal('- 1\n\n- # 2\n\n- ## 3\n\n')
   })
-})
-
-describe('markdown生成树测试', function () {
-  let root = markdownToTree('1. 11\n2. 22\n3. 33')
+  let root4 = markdownToTree('1. 11\n2. 22\n3. 33')
   it('有序列表测试', function () {
-    expect(root.toMarkdown()).to.be.equal('1. 11\n\n2. 22\n\n3. 33\n\n')
+    expect(root4.toMarkdown()).to.be.equal('1. 11\n\n2. 22\n\n3. 33\n\n')
   })
-})
-
-describe('markdown生成树测试', function () {
-  let root = markdownToTree('- [x] 3\n- [ ] 2')
+  let root5 = markdownToTree('- [x] 3\n- [ ] 2')
   it('任务列表测试', function () {
-    expect(root.toMarkdown()).to.be.equal('- [x] 3\n\n- [ ] 2\n\n')
+    expect(root5.toMarkdown()).to.be.equal('- [x] 3\n\n- [ ] 2\n\n')
   })
 })
-
