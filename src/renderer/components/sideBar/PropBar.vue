d31882cf5f68f65ac65d1e3525dfe71b378df9b4
Chenjunjie <2624882033@qq.com>
2023-04-18 01:14:44 +0800

    feat: 基础布局改进

---
+++ src/renderer/components/sideBar/PropBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "PropBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/components/sideBar/TagBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TagBar"
+}
+</script>
+
+<style scoped>
+
+</style>
