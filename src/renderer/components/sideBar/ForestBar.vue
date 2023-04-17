958e6bd863f098126fc5f65d298c9c8de848352b
Chenjunjie <2624882033@qq.com>
2023-04-18 01:14:44 +0800

    feat: 基础布局改进

---
+++ src/renderer/components/sideBar/ForestBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ForestBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/components/sideBar/SearchBar.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style scoped>
+
+</style>
