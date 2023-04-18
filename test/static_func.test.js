0acff9b7f7341539e145dae6d28e71265091a75f
Blore <1137480882@qq.com>
2023-04-18 21:55:28 +0800

    style(static_func): 删除无用文件

--- test/static_func.test.js
+++
@@ -1,8 +0,0 @@
-const moduleUnderTest = require('../src/static_func.js')
-const expect = require('chai').expect
-
-describe('同步纯函数测试', function () {
-  it('简单 int 加法', function () {
-    expect(moduleUnderTest.add(1, 1)).to.be.equal(2)
-  })
-})
