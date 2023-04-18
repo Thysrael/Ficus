223a356bec655f5169264e760113f15f68a266f7
wz <dofingert@gmail.com>
2023-04-19 00:03:22 +0800

    fix(preload.js): 修正 preload.js 的载入问题

--- src/background.js
+++ src/background.js
@@ -22,7 +22,7 @@ async function createWindow () {
     height: 600,
     frame: false,
     webPreferences: {
-      preload: path.join(__dirname, '../src/main/filesystem/preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions:{
+    electronBuilder: {
+      preload: 'src/main/filesystem/preload.js'
+    }
+  }
 })
