1bcc915a12e442da44939979feb4c80d471f9356
Chenjunjie <2624882033@qq.com>
2023-04-18 14:39:18 +0800

    feat: 完善了布局页面

--- src/renderer/components/sideBar/GraphBar.vue
+++ src/renderer/components/sideBar/GraphBar.vue
@@ -1,7 +1,25 @@
 <template>
-<div>
-  我是GraphBar
-</div>
+  <div>
+    <div style="font-size: 12px">
+      节点详情信息
+    </div>
+    <div style="font-size: 10px">
+      文件名：中文测试.md
+    </div>
+    <hr style="border: none;border-top: 2px solid #ccc;height: 1px;margin: 20px 0;">
+    <div style="font-size: 12px">
+      操作节点
+    </div>
+    <button style="width: 60px;height: 20px;border-radius: 3px;opacity: 1;background-color: #42b983;font-size: 12px">
+      焦点前进
+    </button>
+    <button style="margin-left: 10px; width: 60px;height: 20px;border-radius: 3px;opacity: 1;background-color: #42b983;font-size: 12px">
+      焦点后退
+    </button>
+    <button style="width: 130px;height: 20px;border-radius: 3px;opacity: 1;background-color: #42b983;font-size: 12px">
+      转换为Ficus根
+    </button>
+  </div>
 </template>
 
 <script>
