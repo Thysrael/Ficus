eb605628b097533d005e28bd8f776193e7bfde4d
Blore <1137480882@qq.com>
2023-04-18 21:50:11 +0800

    feat(ir): 增加mindjson和history

--- src/IR/manager/index.js
+++ src/IR/manager/index.js
@@ -1,24 +1,45 @@
-const { markdownToTree } = require('../block/factory/markdownToTree')
+import { IRTree } from '../component/tree'
+
+const DEFAULT_OPTIONS = {
+  replaced: false
+}
 
 class DataManager {
   constructor () {
     this.tree = undefined
     this.forest = undefined
     this.graph = undefined
+
+    this.treeSet = {}
   }
 
   /**
-     * 从markdown生成FicusTree
-     * @param markdown md文本
-     * @param replaced 是否替换当前tree
-     * @returns
-     */
-  buildTreeFromMarkdown (markdown, replaced = false) {
-    const newTree = markdownToTree(markdown)
-    if (replaced) {
+   * 从markdown生成FicusTree
+   * @param markdown md文本
+   * @param replaced 是否替换当前tree
+   * @returns
+   */
+  buildTreeFromMarkdown (markdown, options = {}) {
+    const treeOptions = Object.assign({}, DEFAULT_OPTIONS, options)
+    let newTree
+    if (this.treeSet[markdown.absolutePath] !== undefined) {
+      newTree = this.treeSet[markdown.absolutePath]
+    } else {
+      newTree = new IRTree({ markdown })
+    }
+
+    if (treeOptions.replaced) {
       this.tree = newTree
     }
-    return newTree
+    return newTree.tree
+  }
+
+  updateTreeFromMarkdown (markdown) {
+    if (this.tree === undefined) {
+      console.log('不存在当前文件')
+    } else {
+      this.tree.update({ markdown })
+    }
   }
 
   /**
@@ -37,6 +58,46 @@ class DataManager {
    * @returns 大纲json
    */
   getTreeOutline () {
+    if (this.tree === undefined) {
+      return {}
+    }
+    return this.tree.toOutlineJson()
+  }
+
+  /**
+   * 从mindJson生成FicusTree
+   * @param mindJson md文本
+   * @param replaced 是否替换当前tree
+   * @returns
+   */
+  buildTreeFromMindJson (mindJson, options = {}) {
+    const treeOptions = Object.assign({}, DEFAULT_OPTIONS, options)
+    let newTree
+    if (this.treeSet[mindJson.absolutePath] !== undefined) {
+      newTree = this.treeSet[mindJson.absolutePath]
+    } else {
+      newTree = new IRTree({ mindJson })
+    }
+
+    if (treeOptions.replaced) {
+      this.tree = newTree
+    }
+    return newTree.tree
+  }
+
+  updateTreeFromMindJson (mindJson) {
+    if (this.tree === undefined) {
+      console.log('不存在当前文件')
+    } else {
+      this.tree.update({ mindJson })
+    }
+  }
+
+  /**
+   *
+   * @returns mind json
+   */
+  getTreeMindJson () {
     if (this.tree === undefined) {
       return {}
     }
