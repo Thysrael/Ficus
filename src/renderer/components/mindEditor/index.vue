d49ab0d7d17334a4d28e7a3964615f1f6991e8f8
GwokHiujin <guozi1625113688@163.com>
2023-04-20 01:14:27 +0800

    feat(./src/renderer/components/mindeditor): 添加了Ficus导图的样式及相关插件

---
+++ src/renderer/components/mindEditor/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <FicTree/>
+</template>
+
+<script>
+import FicTree from '@/renderer/components/mindEditor/FicTree.vue'
+
+export default {
+  name: 'index',
+  components: { FicTree },
+}
+</script>
+
+<style scoped>
+
+</style>
