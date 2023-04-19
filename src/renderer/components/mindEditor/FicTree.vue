cf14e9ad263b3bd4fdfc8273bb7d19e4104dd619
GwokHiujin <guozi1625113688@163.com>
2023-04-20 01:14:27 +0800

    feat(./src/renderer/components/mindeditor): 添加了Ficus导图的样式及相关插件

--- src/renderer/components/mindEditor/FicTree.vue
+++ src/renderer/components/mindEditor/FicTree.vue
@@ -7,31 +7,33 @@
            download-btn="true"
            timetravel="true"
            ctm="true"
-           update:model-value="onChange"
+           @update:model-value="onChange"
+           @select="onSelect"
   >
   </mindmap>
 </template>
 
 <script>
-<<<<<<< HEAD
 import mindmap from 'ficus-mindmap'
 import 'ficus-mindmap/dist/style.css'
-=======
->>>>>>> f68fd58 (feat(./src/renderer/components/mindeditor): 添加了ficus-mindmap插件包)
 import testMind from './test_mind.json'
 import { defineComponent, ref } from 'vue'
-import mindmap from 'ficus-mindmap'
-import 'ficus-mindmap/dist/style.css'
 export default defineComponent({
   components: {
     mindmap
   },
   setup () {
     const data = ref(testMind)
-    const onChange = () => console.log('text')
+    const onChange = (msg) => {
+      console.log(msg)
+    }
+    const onSelect = (msg) => {
+      console.log(msg)
+    }
     return {
       data,
-      onChange
+      onChange,
+      onSelect
     }
   }
 })
