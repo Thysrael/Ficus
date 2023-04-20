884b16486b6c6b0a7a0663484bf2852549c043f2
Blore <1137480882@qq.com>
2023-04-20 23:54:26 +0800

    test(tags): 增加tags相关测试

---
+++ test/IR/factory/tag.test.js
@@ -0,0 +1,53 @@
+
+const { IRTree } = require('../../../src/IR/component/tree')
+const assert = require('assert')
+
+describe('已有tag', function () {
+  const tree = new IRTree({ content: '---\ntags:\n  - a\n  - b\n---\n\n' })
+  it('获得tag', function () {
+    assert.deepStrictEqual(tree.getTags(), ['a', 'b'])
+  })
+  it('增加重复tag', function () {
+    tree.addTag('a')
+    assert.deepStrictEqual(tree.getTags(), ['a', 'b'])
+  })
+  it('增加不重复tag', function () {
+    tree.addTag('c')
+    assert.deepStrictEqual(tree.getTags(), ['a', 'b', 'c'])
+  })
+  it('移除tag', function () {
+    tree.removeTag('b')
+    assert.deepStrictEqual(tree.getTags(), ['a', 'c'])
+  })
+  it('移除不存在tag', function () {
+    tree.removeTag('b')
+    assert.deepStrictEqual(tree.getTags(), ['a', 'c'])
+  })
+  it('移除所有tag', function () {
+    tree.removeTag('a')
+    tree.removeTag('c')
+    assert.deepStrictEqual(tree.getTags(), [])
+  })
+  it('增加tag', function () {
+    tree.addTag('c')
+    assert.deepStrictEqual(tree.getTags(), ['c'])
+  })
+})
+
+describe('无tag', function () {
+  const tree = new IRTree({ content: '' })
+  it('初始tag', function () {
+    assert.deepStrictEqual(tree.getTags(), [])
+  })
+  it('增加tag', function () {
+    tree.addTag('a')
+    assert.deepStrictEqual(tree.getTags(), ['a'])
+  })
+  it('第二次增加tag', function () {
+    tree.addTag('b')
+    assert.deepStrictEqual(tree.getTags(), ['a', 'b'])
+  })
+  it('测试输出md', function () {
+    assert.deepStrictEqual(tree.toMarkdown(), '---\ntags:\n  - a\n  - b\n---\n\n')
+  })
+})
