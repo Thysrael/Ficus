ee1fc001e7f85b144c3b1f8c6b68fb257c5d30fd
Chenjunjie <2624882033@qq.com>
2023-04-21 03:54:52 +0800

    fix: 通过eslint

--- test/IR/IRToMindjson.test.js
+++ test/IR/IRToMindjson.test.js
@@ -3,6 +3,7 @@ const chai = require('chai')
 const assert = require('assert')
 
 describe('IR到MindJson测试', function () {
+  console.log(chai)
   const root = markdownToTree('# aa')
   it('简单单行测试', function () {
     assert.deepStrictEqual(root.toMindJson(), {
