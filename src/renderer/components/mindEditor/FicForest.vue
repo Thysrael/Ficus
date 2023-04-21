76a14b44ad7e8a59634b434a0c0be65a31113ee3
GwokHiujin <guozi1625113688@163.com>
2023-04-21 14:07:35 +0800

    fix(mindeditor): 修复了npm包无法识别的问题

--- src/renderer/components/mindEditor/FicForest.vue
+++ src/renderer/components/mindEditor/FicForest.vue
@@ -1,13 +1,33 @@
 <template>
-
+  <mindmap v-model="data"
+           drag="true"
+           zoom="true"
+           center-btn="true"
+           fit-btn="true"
+           download-btn="true"
+           timetravel="true"
+           ctm="true"
+           update:model-value="onChange"
+  >
+  </mindmap>
 </template>
 
 <script>
-export default {
-  name: 'FicForest'
-}
+import testMind from './test_mind.json'
+import { defineComponent, ref } from 'vue'
+import mindmap from 'ficus-mindmap'
+import 'ficus-mindmap/dist/style.css'
+export default defineComponent({
+  components: {
+    mindmap
+  },
+  setup () {
+    const data = ref(testMind)
+    const onChange = () => console.log('text')
+    return {
+      data,
+      onChange
+    }
+  }
+})
 </script>
-
-<style scoped>
-
-</style>
