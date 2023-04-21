e30afc4d506bcf1045f7c82f544edcae6cbbe84d
GwokHiujin <guozi1625113688@163.com>
2023-04-21 14:07:35 +0800

    fix(mindeditor): 修复了npm包无法识别的问题

--- src/renderer/components/mindEditor/index.vue
+++ src/renderer/components/mindEditor/index.vue
@@ -4,10 +4,26 @@
 
 <script>
 import FicTree from '@/renderer/components/mindEditor/FicTree.vue'
+import FicForest from '@/renderer/components/mindEditor/FicForest.vue'
+import FicGraph from '@/renderer/components/mindEditor/FicGraph.vue'
+import bus from 'vue3-eventbus'
 
 export default {
-  name: 'index',
-  components: { FicTree },
+  name: 'MindMap',
+  props: {
+    content: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    FicTree,
+    FicForest,
+    FicGraph
+  },
+  setup(props) {
+    // TODO
+  }
 }
 </script>
 
