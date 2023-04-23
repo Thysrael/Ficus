40c9efa21c091b879ab1e2ea749a3eb61f018f20
Chenjunjie <2624882033@qq.com>
2023-04-24 00:02:59 +0800

    feat: 合并重命名分支

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -35,7 +35,109 @@ export default {
   name: 'App',
   components: { SideBar, MyHeader, TextArea },
   setup () {
-    const data = ref([])
+    const data = ref([{
+      name: 'app2',
+      curChild: -1,
+      path: 'app2',
+      absolutePath: ['app2'],
+      offset: -1,
+      content: '# 1',
+      type: 'folder',
+      children: [{
+        name: 'src',
+        curChild: -1,
+        path: 'app2\\src',
+        absolutePath: ['app2', 'src'],
+        offset: -1,
+        content: '- 2',
+        type: 'folder',
+        children: [{
+          name: 'components',
+          curChild: -1,
+          path: 'app2\\src\\components',
+          absolutePath: ['app2', 'src', 'components'],
+          offset: -1,
+          content: '3',
+          type: 'folder',
+          children: [{
+            name: 'FileNav.vue',
+            curChild: -1,
+            path: 'app2\\src\\components\\FileNav.vue',
+            absolutePath: ['app2', 'src', 'components', 'FileNav.vue'],
+            offset: -1,
+            content: '4',
+            children: [],
+            type: 'file'
+          }, {
+            name: 'FileNavItem.vue',
+            curChild: -1,
+            path: 'app2\\src\\components\\FileNavItem.vue',
+            absolutePath: ['app2', 'src', 'components', 'FileNavItem.vue'],
+            offset: -1,
+            content: '5',
+            children: [],
+            type: 'file'
+          }]
+        }, {
+          name: 'assets',
+          curChild: -1,
+          path: 'app2\\src\\assets',
+          absolutePath: ['app2', 'src', 'assets'],
+          offset: -1,
+          content: '6',
+          type: 'folder',
+          children: [{
+            name: 'logo.png',
+            curChild: -1,
+            path: 'app2\\src\\assets\\logo.png',
+            absolutePath: ['app2', 'src', 'assets', 'logo.png'],
+            offset: -1,
+            content: '7',
+            children: [],
+            type: 'file'
+          }, {
+            name: 'tailwind.css',
+            curChild: -1,
+            path: 'app2\\src\\assets\\tailwind.css',
+            absolutePath: ['app2', 'src', 'assets', 'tailwind.css'],
+            offset: -1,
+            content: '8',
+            children: [],
+            type: 'file'
+          }, {
+            name: 'FileNav.vue',
+            curChild: -1,
+            path: 'app2\\src\\assets\\FileNav.vue',
+            absolutePath: ['app2', 'src', 'assets', 'FileNav.vue'],
+            offset: -1,
+            content: '9',
+            children: [],
+            type: 'file'
+          }]
+        }, {
+          name: 'FileNav.vue',
+          curChild: -1,
+          path: 'app2\\src\\FileNav.vue',
+          absolutePath: ['app2', 'src', 'FileNav.vue'],
+          offset: -1,
+          content: '```vue\n' +
+          'setup() {\n' +
+          '\tlet name = \'xxx\',\n' +
+          '\tlet age = 18\n' +
+          '\t\n' +
+          '\n' +
+          '\treturn {\n' +
+          '\t\tname,\n' +
+          '\t\tage,\n' +
+          '\t\tsayHello\n' +
+          '\t}\t\n' +
+          '}\n' +
+          '```',
+          children: [],
+          type: 'file'
+        }]
+      }]
+    }])
     const source = ref({}) // 源对象
     const dst = ref({}) // 目标对象
     const windowHeight = ref(window.innerHeight + 'px')
@@ -214,6 +316,7 @@ export default {
 
     bus.on('openDir', (obj) => {
       data.value = [obj]
+      console.log(data.value)
     })
 
     function buildNewFileFromOld (father, curObj) {
