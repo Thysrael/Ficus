22717fdc739615f329747c47e041139887d9d64d
Bruce <brucejrue25@gmail.com>
2023-04-25 01:38:41 +0800

    feat: 添加粘贴函数

--- src/main/filesystem/general.js
+++ src/main/filesystem/general.js
@@ -1,6 +1,54 @@
 const { getTree } = require('./getFileTree')
 const path = require('path')
 const fs = require('fs-extra')
+const { dialog } = require('electron')
+
+function copyDir (srcDir, desDir) {
+  // eslint-disable-next-line n/handle-callback-err
+  fs.readdir(srcDir, { withFileTypes: true }, (err, files) => {
+    for (const file of files) {
+      // 判断是否为文件夹
+      if (file.isDirectory()) {
+        const dirS = path.resolve(srcDir, file.name)
+        const dirD = path.resolve(desDir, file.name)
+        // 判断是否存在dirD文件夹
+        if (!fs.existsSync(dirD)) {
+          fs.mkdir(dirD, (err) => {
+            if (err) console.log(err)
+          })
+        }
+        copyDir(dirS, dirD)
+      } else {
+        const srcFile = path.resolve(srcDir, file.name)
+        const desFile = path.resolve(desDir, file.name)
+        fs.copyFileSync(srcFile, desFile)
+      }
+    }
+  })
+}
+exports.pasteDir = (srcDir, desDir) => {
+  copyDir(srcDir, desDir)
+}
+exports.pasteFile = async (srcDir, desDir) => {
+  // 如果该路径下不存在同名文件则复制该文件
+  if (!fs.existsSync(desDir)) {
+    fs.promises.copyFile(srcDir, desDir)
+  } else {
+    await dialog.showMessageBox({
+      type: 'error', // 图标类型
+      title: '错误', // 信息提示框标题
+      message: `当前目录下已有同名文件:${desDir}`, // 信息提示框内容
+      buttons: ['跳过', '覆盖'], // 下方显示的按钮
+      cancelId: 2// 点击x号关闭返回值
+    }).then((index) => {
+      if (index.response === 1) {
+        fs.copyFileSync(srcDir, desDir)
+      } else {
+        // console.log('skip')
+      }
+    })
+  }
+}
 
 function deleteDir (url) {
   let files = []
