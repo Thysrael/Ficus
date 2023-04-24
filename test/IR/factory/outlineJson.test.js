e581ee5d7d5c8a0943a3a6ff4232d785adaf7beb
Blore <1137480882@qq.com>
2023-04-24 23:26:49 +0800

    feat: 增加全局link解析，并优化代码

--- test/IR/factory/outlineJson.test.js
+++ test/IR/factory/outlineJson.test.js
@@ -2,8 +2,8 @@ const { markdownToTree } = require('../../../src/IR/block/factory/markdownToTree
 const assert = require('assert')
 
 describe('markdown生成树到大纲视图测试', function () {
-  const root = markdownToTree('# aa')
   it('简单单行测试', function () {
+    const root = markdownToTree('# aa')
     assert.deepStrictEqual(root.toOutlineJson(), {
       name: 'root',
       level: 0,
@@ -14,6 +14,14 @@ describe('markdown生成树到大纲视图测试', function () {
           children: []
         }
       ]
+    })
+  })
+  it('空串测试', function () {
+    const root2 = markdownToTree('')
+    assert.deepStrictEqual(root2.toOutlineJson(), {
+      name: 'root',
+      level: 0,
+      children: []
     })
   })
 })
