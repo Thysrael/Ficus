29995acfdf51850205d0f78fdf885dfc63ab1f49
Blore <1137480882@qq.com>
2023-04-24 22:03:53 +0800

    refactor(filesystem): 重构部分代码，增强可读性

--- src/main/filesystem/index.js
+++
@@ -1,5 +0,0 @@
-// 完成APP初始化相关事件绑定
-class App {
-
-}
-export default App
---
+++ src/main/filesystem/linkManager.js
@@ -0,0 +1,9 @@
+class LinkManager {
+  constructor () {
+    this.tags = new Set()
+  }
+}
+
+const linkManager = new LinkManager()
+
+module.exports = linkManager
