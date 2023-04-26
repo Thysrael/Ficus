cc55356ef4e937d36809420fb0832794686595ca
Chenjunjie <2624882033@qq.com>
2023-04-26 22:24:14 +0800

    fix: 修复了大纲跳转问题

--- src/IR/helper/counter.js
+++ src/IR/helper/counter.js
@@ -1,27 +1,40 @@
 class IdMarker {
   constructor () {
-    this.reset()
+    this._id = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0
+    }
   }
 
-  reset () {
-    this._id = -1
+  reset (beginIndex) {
+    for (let level = beginIndex; level <= 6; level++) {
+      this._id[level] = 0
+    }
   }
 
   /**
      *
-     * @param {children} obj
+     * @param {children: [obj], level: number} obj
      */
   markId (obj) {
-    obj.id = this.allocId()
+    obj.id = this.allocId(obj.level)
     if (obj.children) {
+      this.reset(obj.level + 1)
       for (const child of obj.children) {
         this.markId(child)
       }
     }
   }
 
-  allocId () {
-    return this._id++
+  allocId (level) {
+    const res = this._id[level]
+    this._id[level] = res + 1
+    return res
   }
 }
 
