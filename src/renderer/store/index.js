999ed8485986319e748d55dbf2bc6f6f04c22e9b
Blore <1137480882@qq.com>
2023-04-26 17:42:45 +0800

    fix(store): 回滚xy状态

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,24 +1,40 @@
 import { createStore } from 'vuex'
 
 const state = {
+  xy: {
+    x: 0,
+    y: 0
+  },
   mode: {
     value: -1 // -1表示当前模式为欢迎界面，0表示纯文本，1表示源码，2表示树形，3表示图
   }
 }
 
 const mutations = {
+  // 定义mutations，用于修改状态(同步)
+  updateXY (state, xy) {
+    state.xy = xy
+  },
   updateMode (state, mode) {
     state.mode = mode
   }
 }
 
 const actions = {
+  // 定义actions，用于修改状态(异步)
+  updateXY (context, xy) {
+    context.commit('updateXY', xy)
+  },
   updateMode (context, mode) {
     context.commit('updateMode', mode)
   }
 }
 
 const getters = {
+  // 定义一个getters
+  getXY (state) {
+    return state.xy.x + '+' + state.xy.y
+  },
   getMode (state) {
     return state.mode.value
   }
