f44a1d6f112c982cd466ea98ebee900f6d32a567
Blore <1137480882@qq.com>
2023-05-01 21:23:45 +0800

    style(ipcmain): 简单调整接口写法

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,12 +1,13 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  minWindow: () => ipcRenderer.invoke('window-min'),
-  maxWindow: () => ipcRenderer.invoke('window-max'),
-  closeWindow: () => ipcRenderer.invoke('window-close'),
+  minWindow: () => ipcRenderer.send('window-min'),
+  maxWindow: () => ipcRenderer.send('window-max'),
+  closeWindow: () => ipcRenderer.send('window-close'),
 
-  openDev: () => ipcRenderer.invoke('dev-open'),
-  closeDev: () => ipcRenderer.invoke('dev-close'),
+  openDev: () => ipcRenderer.send('dev-open'),
+  closeDev: () => ipcRenderer.send('dev-close'),
+  
   newFicusVault: () => ipcRenderer.invoke('newProject'),
   openFile: () => ipcRenderer.invoke('dialog:openFile'),
 
