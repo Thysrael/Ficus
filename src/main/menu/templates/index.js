1bb59835e5c947e523d31326a5ae263cf2d3151a
Blore <1137480882@qq.com>
2023-05-18 00:30:21 +0800

    feat(menu): 增加mac的menu展示

--- src/main/menu/templates/index.js
+++ src/main/menu/templates/index.js
@@ -1,11 +1,21 @@
 import { isOsx } from '@/main/config'
 import app from './app'
 import file from './file'
+import edit from './edit'
+import paragraph from './paragraph'
+import format from './format'
+import view from './view'
+import help from './help'
 
 export function getMenuTemplates (keybindings) {
   return [
     ...(isOsx ? [app(keybindings)] : []),
-    file(keybindings)
+    file(keybindings),
+    edit(keybindings),
+    paragraph(keybindings),
+    format(keybindings),
+    view(keybindings),
+    help(keybindings)
   ]
 }
 
---
+++ src/main/menu/templates/view.js
@@ -0,0 +1,51 @@
+export default function (keybindings) {
+  const viewMenu = {
+    label: '视图',
+    submenu: [{
+      label: '文本模式',
+      id: 'view.text-mode',
+      type: 'checkbox',
+      checked: false,
+      accelerator: keybindings.getAccelerator('view.text-mode'),
+      click (menuItem, browserWindow) {
+        browserWindow.webContents.send('ficus::keyboard-event', menuItem.id)
+      }
+    }, {
+      label: '源码模式',
+      id: 'view.source-code-mode',
+      type: 'checkbox',
+      checked: false,
+      accelerator: keybindings.getAccelerator('view.source-code-mode'),
+      click (menuItem, browserWindow) {
+        browserWindow.webContents.send('ficus::keyboard-event', menuItem.id)
+      }
+    }, {
+      label: 'Ficus模式',
+      id: 'view.focus-mode',
+      type: 'checkbox',
+      checked: false,
+      accelerator: keybindings.getAccelerator('view.focus-mode'),
+      click (menuItem, browserWindow) {
+        browserWindow.webContents.send('ficus::keyboard-event', menuItem.id)
+      }
+    }, {
+      label: '开发者工具',
+      id: 'view.toggle-dev-tools',
+      accelerator: keybindings.getAccelerator('view.toggle-dev-tools'),
+      click (menuItem, browserWindow) {
+        browserWindow.webContents.send('ficus::keyboard-event', menuItem.id)
+      }
+    }, {
+      label: '打字机模式',
+      id: 'view.typewriter-mode',
+      type: 'checkbox',
+      checked: false,
+      accelerator: keybindings.getAccelerator('view.typewriter-mode'),
+      click (menuItem, browserWindow) {
+        browserWindow.webContents.send('ficus::keyboard-event', menuItem.id)
+      }
+    }]
+  }
+
+  return viewMenu
+}
