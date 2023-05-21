6fb368cd314b518936eba8b4764516cbe9fe408f
Blore <1137480882@qq.com>
2023-05-21 22:35:07 +0800

    feat: 初步增加在主进程的命令集散

---
+++ src/main/commands/index.js
@@ -0,0 +1,44 @@
+import COMMANDS from '../../common/commands'
+
+class CommandManager {
+  constructor () {
+    this._commands = new Map()
+  }
+
+  add (id, callback) {
+    const { _commands } = this
+    if (_commands.has(id)) {
+      throw new Error(`Command with id="${id}" already exists.`)
+    }
+    _commands.set(id, callback)
+  }
+
+  remove (id) {
+    return this._commands.delete(id)
+  }
+
+  has (id) {
+    return this._commands.has(id)
+  }
+
+  execute (id, ...args) {
+    const command = this._commands.get(id)
+    if (!command) {
+      throw new Error(`No command found with id="${id}".`)
+    }
+    return command(...args)
+  }
+
+  __verifyDefaultCommands () {
+    const { _commands } = this
+    Object.keys(COMMANDS).forEach(propertyName => {
+      const id = COMMANDS[propertyName]
+      if (!_commands.has(id)) {
+        console.error(`[DEBUG] Default command with id="${id}" isn't available!`)
+      }
+    })
+  }
+}
+
+const commandManagerInstance = new CommandManager()
+export { commandManagerInstance as CommandManager }
