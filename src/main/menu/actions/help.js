38a8a615e5df5ec99e8a494b05dc1d4bef54f196
Bruce <brucejrue25@gmail.com>
2023-05-23 11:34:13 +0800

    feat: 整理command-manager

---
+++ src/main/menu/actions/help.js
@@ -0,0 +1,8 @@
+import COMMANDS from '../../../common/commands'
+
+const helpAbout = async (win, id) => {
+  win.webContents.send('ficus::keyboard-event', id)
+}
+export const loadHelpCommands = commandManager => {
+  commandManager.add(COMMANDS.HELP_ABOUT, helpAbout)
+}
---
+++ src/main/menu/actions/view.js
@@ -0,0 +1,23 @@
+import COMMANDS from '../../../common/commands'
+const textMode = async (win, id) => {
+  win.webContents.send('ficus::keyboard-event', id)
+}
+const sourceCodeMode = async (win, id) => {
+  win.webContents.send('ficus::keyboard-event', id)
+}
+const ficusMode = async (win, id) => {
+  win.webContents.send('ficus::keyboard-event', id)
+}
+const toggleDevTools = async (win, id) => {
+  win.webContents.send('ficus::keyboard-event', id)
+}
+const typewriterMode = async (win, id) => {
+  win.webContents.send('ficus::keyboard-event', id)
+}
+export const loadViewCommands = commandManager => {
+  commandManager.add(COMMANDS.VIEW_TEXT_MODE, textMode)
+  commandManager.add(COMMANDS.VIEW_SOURCE_CODE_MODE, sourceCodeMode)
+  commandManager.add(COMMANDS.VIEW_FICUS_MODE, ficusMode)
+  commandManager.add(COMMANDS.VIEW_TOGGLE_DEV_TOOLS, toggleDevTools)
+  commandManager.add(COMMANDS.VIEW_TYPEWRITER_MODE, typewriterMode)
+}
