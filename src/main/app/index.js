359f84ad9fe830931bbe3f456b92a7aea2a9161c
Blore <1137480882@qq.com>
2023-05-23 10:11:25 +0800

    fix: 修复误注释

--- src/main/app/index.js
+++ src/main/app/index.js
@@ -23,9 +23,9 @@ class App {
   }
 
   init () {
-    // this.watcher.on('add', (filepath) => this.linkManager.addFile(filepath))
-    // this.watcher.on('change', (filepath) => this.linkManager.updateFile(filepath))
-    // this.watcher.on('unlink', (filepath) => this.linkManager.removeFile(filepath))
+    this.watcher.on('add', (filepath) => this.linkManager.addFile(filepath))
+    this.watcher.on('change', (filepath) => this.linkManager.updateFile(filepath))
+    this.watcher.on('unlink', (filepath) => this.linkManager.removeFile(filepath))
 
     this.menu.init(this.keyBinding)
     this._createFicusWindow()
