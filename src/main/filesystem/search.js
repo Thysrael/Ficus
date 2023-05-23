97602d2f1983ef75c2631b67eab2c799787c7b7d
Blore <1137480882@qq.com>
2023-05-23 09:26:30 +0800

    feat(search): 增加全局搜索基础支持

---
+++ src/main/filesystem/search.js
@@ -0,0 +1,32 @@
+import path from 'path'
+import fs from 'fs-extra'
+import { isValidFolderPath, isValidMarkdownFilePath } from '../helper/path'
+
+export class SearchEngine {
+  constructor (dirPath, token) {
+    this.files = []
+    this.results = []
+    this._getFilePaths(dirPath)
+  }
+
+  async search (token) {
+    for (const filepath of this.files) {
+      const content = await fs.promises.readFile(filepath)
+      if (content.toString().indexOf(token) !== -1) {
+        this.results.push(filepath)
+      }
+    }
+  }
+
+  _getFilePaths (dirPath) {
+    const subFileOrFolder = fs.readdirSync(dirPath)
+    for (const subItem of subFileOrFolder) {
+      const subItemPath = path.resolve(dirPath, subItem)
+      if (isValidFolderPath(subItemPath)) {
+        this._getFilePaths(subItemPath)
+      } else if (isValidMarkdownFilePath(subItemPath)) {
+        this.files.push(subItemPath)
+      }
+    }
+  }
+}
