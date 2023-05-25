a76cd015747c82cae12565a1491c1c8e9e742340
Bruce <brucejrue25@gmail.com>
2023-05-25 16:30:43 +0800

    feat: 最近打开文件

--- src/main/menu/index.js
+++ src/main/menu/index.js
@@ -1,15 +1,44 @@
 import { Menu, app } from 'electron'
 import { isOsx, isWindows } from '../config'
 import { getMenuTemplates, toRawMenuTemplates } from './templates'
+import fs from 'fs'
+import path from 'path'
+import { ensureDirSync } from '@/main/filesystem/fileManipulate'
+import { isValidFilePath, isValidFolderPath } from '@/main/helper/path'
 
+const userDataPath = app.getPath('userData')
+const RECENTLY_USED_DOCUMENTS_FILE_NAME = 'recently-used-documents.json'
+const MAX_RECENTLY_USED_DOCUMENTS = 12
 class AppMenu {
   constructor (keybinding) {
     this.isOsxOrWindows = isOsx || isWindows
     this.keybinding = keybinding
+    this._userDataPath = userDataPath
+    this.RECENTS_PATH = path.join(userDataPath, RECENTLY_USED_DOCUMENTS_FILE_NAME)
     if (isOsx) {
       Menu.setApplicationMenu(this._buildMenu())
     }
     this._LISTENForIpcMain()
+  }
+
+  getRecentlyUsedDocuments () {
+    const { RECENTS_PATH } = this
+    if (!isValidFilePath(RECENTS_PATH)) {
+      return []
+    }
+
+    try {
+      const recentDocuments = JSON.parse(fs.readFileSync(RECENTS_PATH, 'utf-8'))
+        .filter(f => f && (isValidFilePath(f) || isValidFolderPath(f)))
+
+      if (recentDocuments.length > MAX_RECENTLY_USED_DOCUMENTS) {
+        recentDocuments.splice(MAX_RECENTLY_USED_DOCUMENTS, recentDocuments.length - MAX_RECENTLY_USED_DOCUMENTS)
+      }
+      return recentDocuments
+    } catch (err) {
+      // log.error('Error while read recently used documents:', err)
+      return []
+    }
   }
 
   setWindowRawMenu (win) {
@@ -25,15 +54,48 @@ class AppMenu {
     if (this.isOsxOrWindows) {
       app.addRecentDocument(filePath)
     }
+    if (isOsx) return
+
+    let recentDocuments = this.getRecentlyUsedDocuments()
+    const index = recentDocuments.indexOf(filePath)
+    let needSave = index !== 0
+    if (index > 0) {
+      recentDocuments.splice(index, 1)
+    }
+    if (index !== 0) {
+      recentDocuments.unshift(filePath)
+    }
+
+    if (recentDocuments.length > MAX_RECENTLY_USED_DOCUMENTS) {
+      needSave = true
+      recentDocuments.splice(MAX_RECENTLY_USED_DOCUMENTS, recentDocuments.length - MAX_RECENTLY_USED_DOCUMENTS)
+    }
+
+    // this.updateAppMenu(recentDocuments)
+    recentDocuments = this.getRecentlyUsedDocuments()
+    if (needSave) {
+      ensureDirSync(this._userDataPath)
+      const json = JSON.stringify(recentDocuments, null, 2)
+      fs.writeFileSync(this.RECENTS_PATH, json, 'utf-8')
+    }
   }
 
   /**
      * 清除最近使用的文件路径
      */
   clearRecentlyUsedDocument () {
-    if (this.isOsxOrWindows) {
-      app.clearRecentDocuments()
-    }
+    // if (this.isOsxOrWindows) {
+    //   app.clearRecentDocuments()
+    // }
+    const { isOsxOrWindows, RECENTS_PATH } = this
+    if (isOsxOrWindows) app.clearRecentDocuments()
+    if (isOsx) return
+
+    const recentDocuments = this.getRecentlyUsedDocuments()
+    // this.updateAppMenu(recentDocuments)
+    const json = JSON.stringify(recentDocuments, null, 2)
+    ensureDirSync(this._userDataPath)
+    fs.writeFileSync(RECENTS_PATH, json, 'utf-8')
   }
 
   _buildMenu () {
