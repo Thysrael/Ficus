8ba86e7ced303baa29a8e14526b260abeb59a3f7
DESKTOP-415OV3U\dofingert <dofingert@gmail.com>
2023-06-02 20:43:49 +0800

    fix(static): 支持在 serve 下打开 static 文件夹

--- src/main/menu/config.js
+++ src/main/menu/config.js
@@ -1,7 +1,12 @@
 import path from 'path'
 
 const builtInDocumentsFileName = ['功能规格说明书.md']
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 export function getBuiltInDocumentsPath () {
-  return builtInDocumentsFileName.map(filename => path.resolve(__dirname, './static/docs', filename))
+  if (isDevelopment) {
+    return builtInDocumentsFileName.map(filename => path.resolve(__dirname, '../public/static', 'docs', filename))
+  } else {
+    return builtInDocumentsFileName.map(filename => path.resolve(__dirname, './static', 'docs', filename))
+  }
 }
