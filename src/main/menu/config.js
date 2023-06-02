3fa9c605b41bba20faf5e273533ee40e61c29bf9
DESKTOP-415OV3U\dofingert <dofingert@gmail.com>
2023-06-02 20:09:26 +0800

    chore(build): 修改static文件位置，挪动到 public 目录下，以打包进 app.asar 文件中

--- src/main/menu/config.js
+++ src/main/menu/config.js
@@ -3,5 +3,5 @@ import path from 'path'
 const builtInDocumentsFileName = ['功能规格说明书.md']
 
 export function getBuiltInDocumentsPath () {
-  return builtInDocumentsFileName.map(filename => path.resolve(__dirname, '../static/docs', filename))
+  return builtInDocumentsFileName.map(filename => path.resolve(__dirname, './static/docs', filename))
 }
